b1589ad2ac558ae0c90dc91597ae84d2ce53e842
edagner <ed.agner@hotmail.com>
2019-06-09 14:45:01 -0400

    got sorter to pass out work to each transforming thread

--- work_config.py
+++ work_config.py
@@ -4,19 +4,27 @@ log_dict = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "standard": {"format": "%(levelname)s %(asctime)s thread_id:%(thread)d %(message)s"
-                     }, 
-        "verbose": {"format": "%(lineno)d in %(filename)s at %(asctime)s: %(message)s"}
+        "thread": {"format": "%(levelname)s %(asctime)s %(funcName)s thread_id:%(thread)d - %(message)s"},
+        "verbose": {"format": "%(lineno)d in %(filename)s at %(asctime)s: %(message)s"},
+        "general": {"format": "%(levelname)s %(asctime)s - %(message)s"}
         },
     "handlers": {
-        "console": {"class": "logging.StreamHandler",
-                    "formatter": "standard",
+        "thread": {"class": "logging.StreamHandler",
+                   "formatter": "thread",
+                   "level": "DEBUG"
+                   },
+        "general": {"class": "logging.StreamHandler",
+                    "formatter": "general",
                     "level": "DEBUG"
                     }
         },
     "loggers": {
-        "app": {
-            "handlers": ["console"],
+        "thread": {
+            "handlers": ["thread"],
+            "level": "DEBUG",
+        },
+        "general": {
+            "handlers": ["general"],
             "level": "DEBUG",
         }
     }
--- work_sorter.py
+++ work_sorter.py
@@ -1,11 +1,12 @@
 import logging
 import time
 import threading
-import queue
+from queue import Queue
 from work_config import *
 
 logging.config.dictConfig(log_dict)
-log = logging.getLogger("app")
+log = logging.getLogger("thread")
+logging = logging.getLogger("general")
 
 
 class Job:
@@ -15,13 +16,13 @@ class Job:
         self.jobs_finished = value[1][1]
         self.to_do = [x for x in self.job_order if x not in self.jobs_finished]
         self.applied_job_order = list()
-        self.current_job = self.to_do[0]
+        self.current_job = self.to_do.pop(0) if len(self.to_do) > 0 else None
         self.done = False
 
     def finish_job(self):
-        finished_job = self.to_do.pop(0) if len(self.to_do) > 0 else None
-        log.debug("{} just finished task {}. On to {}".format(self.value, self.current_job, finished_job))
-        self.current_job = finished_job
+        finished_job = self.current_job
+        self.current_job = self.to_do.pop(0) if len(self.to_do) > 0 else None
+        log.debug("{} just finished task {}. On to {}".format(self.value, finished_job, self.current_job))
 
     def complete(self):
         self.done = True
@@ -48,7 +49,8 @@ def multi(multi_q, sort_q):
         if job == "Poison":
             multi_q.put(job)
             break
-        job.finished_job_order.append(1)
+        log.debug("Working on job: {}".format(job.value))
+        job.applied_job_order.append(1)
         job.finish_job()
         sort_q.put(job)
 
@@ -61,7 +63,8 @@ def add(add_q, sort_q):
         if job == "Poison":
             add_q.put(job)
             break
-        job.finished_job_order.append(2)
+        log.debug("Working on job: {}".format(job.value))
+        job.applied_job_order.append(2)
         job.finish_job()
         sort_q.put(job)
 
@@ -74,7 +77,8 @@ def div(div_q, sort_q):
         if job == "Poison":
             div_q.put(job)
             break
-        job.finished_job_order.append(3)
+        log.debug("Working on job: {}".format(job.value))
+        job.applied_job_order.append(3)
         job.finish_job()
         sort_q.put(job)
 
@@ -87,12 +91,13 @@ def minus(minus_q, sort_q):
         if job == "Poison":
             minus_q.put(job)
             break
-        job.finished_job_order.append(4)
+        log.debug("Working on job: {}".format(job.value))
+        job.applied_job_order.append(4)
         job.finish_job()
         sort_q.put(job)
 
 
-def sorter(multi_q, add_q, div_q, minus_q, sort_q):
+def sorter(multi_q, add_q, div_q, minus_q, sort_q, finished_list, job_num):
     while True:
         if sort_q.qsize() == 0:
             time.sleep(1)
@@ -110,7 +115,70 @@ def sorter(multi_q, add_q, div_q, minus_q, sort_q):
             minus_q.put(job)
         else:
             job.complete()
+            finished_list.append(job)
+            if len(finished_list) == job_num:
+                tkill = "Poison"
+                multi_q.put(tkill)
+                add_q.put(tkill)
+                div_q.put(tkill)
+                minus_q.put(tkill)
+                sort_q.put(tkill)
 
 
 if __name__ == "__main__":
-    work = generate_work()
+    work_list = generate_work()
+
+    multi_queue = Queue(10)
+    add_queue = Queue(10)
+    div_queue = Queue(10)
+    minus_queue = Queue(10)
+    sorter_queue = Queue(10)
+
+    finished_job_list = list()
+
+    logging.debug(work_list)
+
+    # current_jobs = list()
+    # job_limit = 3
+
+    # while len(work_list) != 0:
+    #     current_jobs.append(work_list.pop(0))
+    #     if len(current_jobs) =120= 3:
+    #         for i in range(2):
+
+    sort_thread = threading.Thread(
+        target=sorter,
+        args=(multi_queue, add_queue, div_queue, minus_queue, sorter_queue, finished_job_list, len(work_list))
+        )
+    sort_thread.start()
+
+    for name, work in work_list.items():
+        unit = (name, work)
+        logging.debug(unit)
+        job = Job(unit)
+        sorter_queue.put(job)
+
+    thread_list = list()
+
+    for i in range(2):
+        multi_thread = threading.Thread(target=multi, args=(multi_queue, sorter_queue,))
+        add_thread = threading.Thread(target=add, args=(add_queue, sorter_queue,))
+        div_thread = threading.Thread(target=div, args=(div_queue, sorter_queue,))
+        minus_thread = threading.Thread(target=minus, args=(minus_queue, sorter_queue,))
+
+        thread_list.append(multi_thread)
+        thread_list.append(add_thread)
+        thread_list.append(div_thread)
+        thread_list.append(minus_thread)
+
+        multi_thread.start()
+        add_thread.start()
+        div_thread.start()
+        minus_thread.start()
+
+    for t in thread_list:
+        t.join()
+
+    sort_thread.join()
+
+    i = 1
